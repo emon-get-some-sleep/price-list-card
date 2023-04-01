dad8405ac61b5f31a30232657d9eb300f16e9b88
emonh0que <you@example.com>
2023-04-02 00:04:17 +0600

    conditional rendering done

--- src/component/Link/Link.jsx
+++ src/component/Link/Link.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = ({route}) => {
 
     return (
-        <li className='mr-12'>
+        <li className='mr-12 md:py-2 hover:bg-purple-600'>
             <a href={route.path}>{route.navMenu}</a>
         </li>
     );
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link/Link';
-
+import {  Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
     const routes = [
         { id: 1, navMenu: "Home", path: "/home" },
         { id: 2, navMenu: "Contact", path: "/contact" },
@@ -11,11 +13,21 @@ const Navbar = () => {
       ]
       
     return (
-        <nav>
-            <ul className='md:flex'>
+        <nav className='bg-purple-400'>
+             <div className='md:hidden' onClick={() => setOpen(!open)}>
+                <span>{
+                open === true ?
+                <XMarkIcon className="h-6 w-6 text-purple-500" />
+                : <Bars3Icon className="h-6 w-6 text-purple-500" /> 
+                }</span>
+                
+                
+             </div>
+            <ul className={`md:flex pl-8 pb-4 bg-purple-400 duration-1000 absolute sm:static ${open ? 'top-6 py-2' : '-top-48'}`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
+                
             </ul>
         </nav>
     );
